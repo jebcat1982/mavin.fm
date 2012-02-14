9b4b5ffdfe6bbcebbe51028bb1e78afaca421908
Tomer Elmalem <telmalem@gmail.com>
2012-02-13 23:42:16 -0500

    Create a method to make requessts to the bandcamp api and retrieve band, album, and track data.

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,62 @@ class Album < ActiveRecord::Base
   has_many :tracks
   has_many :taggings
   has_many :tags, :through => :taggings
+
+  before_save :get_everything
+
+  def get_everything
+    url_api = URI.parse("http://http://api.bandcamp.com/api/url/1/info?key=#{APIKeys::BANDCAMP}&url=#{self.url}")
+    url_response = Net::HTTP.get(url_api)
+    url_json = JSON.parse(url_response)
+
+    band = Band.find_or_create_by_e_id(url_json["band_id"])
+    band_uri = URI.parse("http://api.bandcamp.com/api/band/3/info?key=#{APIKeys::BANDCAMP}&band_id=#{band.e_id}")
+    band_response = Net::HTTP.get(band_uri)
+    band_json = JSON.parse(band_response)
+
+    band.offsite_url  = band_json["offsite_url"]
+    band.url          = band_json["url"]
+    band.subdomin     = band_json["subdomain"]
+    band.name         = band_json["name"]
+    band.save
+
+    album_uri = URI.parse("http://api.bandcamp.com/api/album/2/info?key=#{APIKeys::BANDCAMP}&album_id=#{url_json["album_id"]}")
+    album_response = Net::HTTP.get(album_uri)
+    album_json = JSON.parse(album_response)
+
+    self.title         = album_json["title"]
+    self.release_date  = album_json["release_date"]
+    self.downloadable  = album_json["downloadable"]
+    self.url           = album_json["url"]
+    self.about         = album_json["about"]
+    self.credits       = album_json["credits"]
+    self.small_art_url = album_json["small_art_url"]
+    self.large_art_url = album_json["large_art_url"]
+    self.artist        = album_json["artist"]
+    self.band_id       = band.id
+    self.e_id          = album_json["album_id"]
+    self.e_band_id     = album_json["band_id"]
+
+    album_json["tracks"].each do |t|
+      #track = JSON.parse(t)
+      self.tracks.build(
+        :title         => track["title"],
+        :number        => track["number"],
+        :duration      => track["duration"],
+        :release_date  => track["release_date"],
+        :downloadable  => track["downloadable"],
+        :url           => track["url"],
+        :streaming_url => track["streaming_url"],
+        :lyrics        => track["lyrics"],
+        :about         => track["about"],
+        :credits       => track["credits"],
+        :small_art_url => track["small_art_url"],
+        :large_art_url => track["large_art_url"],
+        :artist        => track["artist"],
+        :e_id          => track["track_id"],
+        :e_album_id    => track["album_id"],
+        :e_band_id     => track["band_id"]
+      )
+    end
+  end
 end

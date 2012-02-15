879bc6ed67b01db7ebb332d18146743b011dc98a
Tomer Elmalem <telmalem@gmail.com>
2012-02-14 23:02:18 -0500

    Remove the tags from the album factory.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :album do
     url  'http://featurelessghost.bandcamp.com/album/new-moods'
-    tags '2011 dance dark electronic featureless ghost ghost house new wave synthpop Atlanta'
   end
 end

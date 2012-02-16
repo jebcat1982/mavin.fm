8c60db20a3e2d92ba39a13ef5d07043475fc7ac7
Tomer Elmalem <telmalem@gmail.com>
2012-02-15 19:31:08 -0500

    Test to see that a band_id is returned from the URL module.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 describe Album do
   use_vcr_cassette
   
+  describe "bandcamp modules" do
+    it "should retrieve a band_id from the URL module" do
+      album = Factory.build(:album, url: 'http://featurelessghost.bandcamp.com/album/new-moods')
+      info = album.url_module
+      info["band_id"].should == "2153716647"
+    end
+  end
+
   it "creates an album" do
     album = Factory.build(:album)
     album.save

5b2cffe43ccf129b4752bdc081ec648546e37fb1
Tomer Elmalem <telmalem@gmail.com>
2012-02-20 11:44:47 -0500

    Add a playlist factory.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :album do
     url  'http://featurelessghost.bandcamp.com/album/new-moods'
   end
+
+  factory :playlist do
+    search_term 'alternative'
+  end
 end

6b0934fe6d37c891bef0fa8ff7721b3cadc37995
Tomer Elmalem <telmalem@gmail.com>
2012-02-20 11:15:48 -0500

    Remove all unnecessary actions from the albums controller and use the respond_to/respond_with methods to clean things up a bit.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,34 +1,14 @@
 class AlbumsController < ApplicationController
-  def index
-    @albums = Album.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @albums }
-    end
-  end
+  respond_to :json, :except => :create
 
   def show
-    @album = Album.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @album }
-    end
+    respond_with Album.find(params[:id])
   end
 
   def new
     @album = Album.new
     @album.tags.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @album }
-    end
-  end
-
-  def edit
-    @album = Album.find(params[:id])
+    respond_with @album
   end
 
   def create
@@ -41,38 +21,10 @@ class AlbumsController < ApplicationController
       end
     end
 
-    respond_to do |format|
-      if @album.save
-        format.html { redirect_to @album, notice: 'Album was successfully created.' }
-        format.json { render json: @album, status: :created, location: @album }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    @album = Album.find(params[:id])
-
-    respond_to do |format|
-      if @album.update_attributes(params[:album])
-        format.html { redirect_to @album, notice: 'Album was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @album = Album.find(params[:id])
-    @album.destroy
-
-    respond_to do |format|
-      format.html { redirect_to albums_url }
-      format.json { head :no_content }
+    if @album.save
+      redirect_to @album, notice: 'Album was successfully created.'
+    else
+      render action: "new"
     end
   end
 end

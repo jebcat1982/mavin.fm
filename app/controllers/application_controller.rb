d3d7eae29dff4a9ff1cf0624c3a20850f846b24d
Tomer Elmalem <telmalem@gmail.com>
2012-02-20 22:40:07 -0500

    Create a cookie with a session id and store it. Use it every time the user visits the site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def create_session
+    unless cookies[:restore_session]
+      cookies[:restore_session] = (Digest::SHA2.new << Time.now.to_s).to_s
+    end
+  end
+
+  def establish_session
+    unless session[:current]
+      session[:current] = current_user || cookies[:restore_session] 
+    end
+  end
+
+  def current_session
+    session[:current]
+  end
 end
--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -1,4 +1,6 @@
 class ListenController < ApplicationController
+  before_filter :create_session, :establish_session
+
   def index
   end
 end

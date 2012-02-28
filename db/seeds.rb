e3142a936f465aed9976805648b99167075eb318
Tomer Elmalem <telmalem@gmail.com>
2012-02-27 23:33:57 -0500

    Update the album links.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
 albums = []
 albums << {"http://featurelessghost.bandcamp.com/album/biologically-sound-cyber-bodies" => "acid,crash symbols,dance,electronic,ghost,pop,sci-fi,science fiction,synthpop,techno,Atlanta"}
 albums << {"http://featurelessghost.bandcamp.com/album/new-moods" => "2011,dance,dark,electronic,featureless ghost,ghost,house,new wave,synthpop,Atlanta"}
-albums << {"http://supergiantgames.bandcamp.com/" => "acoustic frontier triphop,bastion,soundtrack,supergiant,videogame,San Jose,triphop"}
-albums << {"http://theukeladies.bandcamp.com/" => "dreamscape,faux,hawaiian,future,retro,tropical,Melbourne"}
+albums << {"http://supergiantgames.bandcamp.com/album/bastion-original-soundtrack" => "acoustic frontier triphop,bastion,soundtrack,supergiant,videogame,San Jose,triphop"}
+albums << {"http://theukeladies.bandcamp.com/album/the-ukeladies" => "dreamscape,faux,hawaiian,future,retro,tropical,Melbourne"}
 albums << {"http://pickwick.bandcamp.com/album/myths-vol-1" => "r&b,alternative,pop,rock,soul,Seattle"}
-albums << {"http://woodspider.bandcamp.com/" => "diy folk, folk punk, roots, vaudeville, accordion, acoustic, banjo, birthquake records, cello, freak folk, free download, lo-fi, new england gothic, ukuleler,New York"}
-albums << {"http://3crates.bandcamp.com/" => "hip hop/rap, hip hop, pop, rock, soul, Philadelphia"}
+albums << {"http://woodspider.bandcamp.com/album/in-the-thick-of-it" => "diy folk, folk punk, roots, vaudeville, accordion, acoustic, banjo, birthquake records, cello, freak folk, free download, lo-fi, new england gothic, ukuleler,New York"}
 albums << {"http://treelines.bandcamp.com/album/self-titled" => "indie, rock, Vancouver"}
 albums << {"http://questionsound.bandcamp.com/album/re-deux" => "beats, instrumentals, jazzy, hip hop, soul-jazz, California"}
-albums << {"http://thewillowandthebuilder.bandcamp.com/" => "adrian simon, folk, middletown, new haven, richard miron, the willow and the builder, wesleyan, yale, folk, indie, rock, Connecticut"}
+albums << {"http://thewillowandthebuilder.bandcamp.com/album/the-willow-the-builder" => "adrian simon, folk, middletown, new haven, richard miron, the willow and the builder, wesleyan, yale, folk, indie, rock, Connecticut"}
 
 albums.each do |url,tags|
   album = Album.new(:url => url)

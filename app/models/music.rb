9081ae3d5237b43aa73188992a9927429a5aeaa8
Tomer Elmalem <telmalem@gmail.com>
2012-03-04 21:01:09 -0500

    Get the album info if the bandcamp track has an album id

--- app/models/music.rb
+++ app/models/music.rb
@@ -58,8 +58,9 @@ class Music
 
   # Takes the album_id from the url_module and makes a request to the album module. This retrieves
   # all of the album information such as the artwork, free or not, and all of the album's tracks.
-  def album_module
-    uri = URI.parse("http://api.bandcamp.com/api/album/2/info?key=#{APIKeys::BANDCAMP}&album_id=#{@info["album_id"]}")
+  def album_module(id = nil)
+    album_id = id || @info["album_id"]
+    uri = URI.parse("http://api.bandcamp.com/api/album/2/info?key=#{APIKeys::BANDCAMP}&album_id=#{album_id}")
     response = Net::HTTP.get(uri)
     @album_info = JSON.parse(response)
   end
@@ -101,6 +102,7 @@ class Music
     url_module()
     band_module()
     track_module()
+    album_module(@track_info['album_id']) if @track_info['album_id']
   end
 
   def build_tracks

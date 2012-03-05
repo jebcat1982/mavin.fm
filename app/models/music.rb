0a2269aeafb3bc8781ba611ba3b549294d7e1931
Tomer Elmalem <telmalem@gmail.com>
2012-03-05 10:54:00 -0500

    Refactor the soundcloud track creation to remove duplicate code.

--- app/models/music.rb
+++ app/models/music.rb
@@ -28,47 +28,33 @@ class Music
 
     if json['tracks']
       json['tracks'].each do |track|
-        @track = Track.new
-
-        @track.title          = json['title']
-        @track.duration       = json['duration']
-        @track.downloadable   = json['downloadable']
-        @track.url            = json['permalink_url']
-        @track.streaming_url  = json['stream_url']
-        @track.about          = json['description']
-        @track.small_art_url  = json['artwork_url']
-        @track.large_art_url  = json['artwork_url']
-        @track.artist         = json['user']['username']
-        @track.e_id           = json['id']
-        @track.e_band_id      = json['user-id']
-
-        @tags.each do |tag|
-          @track.taggings.build(:tag_id => tag.id)
-        end
-
-        @track.save
+        soundcloud_track(track)
       end
     else
-      @track = Track.new
-
-      @track.title          = json['title']
-      @track.duration       = json['duration']
-      @track.downloadable   = json['downloadable']
-      @track.url            = json['permalink_url']
-      @track.streaming_url  = json['stream_url']
-      @track.about          = json['description']
-      @track.small_art_url  = json['artwork_url']
-      @track.large_art_url  = json['artwork_url']
-      @track.artist         = json['user']['username']
-      @track.e_id           = json['id']
-      @track.e_band_id      = json['user-id']
-
-      @tags.each do |tag|
-        @track.taggings.build(:tag_id => tag.id)
-      end
-
-      @track.save
+      soundcloud_track(json)
     end
+  end
+
+  def soundcloud_track(json)
+    track = Track.new
+
+    track.title          = json['title']
+    track.duration       = json['duration']
+    track.downloadable   = json['downloadable']
+    track.url            = json['permalink_url']
+    track.streaming_url  = json['stream_url']
+    track.about          = json['description']
+    track.small_art_url  = json['artwork_url']
+    track.large_art_url  = json['artwork_url']
+    track.artist         = json['user']['username']
+    track.e_id           = json['id']
+    track.e_band_id      = json['user-id']
+
+    @tags.each do |tag|
+      track.taggings.build(:tag_id => tag.id)
+    end
+
+    track.save
   end
 
   def split_tags

3abb23db39f1c72da6778c91af7fa7c4d76f8ca6
Tomer Elmalem <telmalem@gmail.com>
2012-03-04 21:58:16 -0500

    Create all of the tracks in a soundcloud playlist.

--- app/models/music.rb
+++ app/models/music.rb
@@ -26,7 +26,27 @@ class Music
     json = JSON.parse(response)
 
     if json['tracks']
-      # soundcloud playlist so create multiple tracks
+      json['tracks'].each do |track|
+        @track = Track.new
+
+        @track.title          = json['title']
+        @track.duration       = json['duration']
+        @track.downloadable   = json['downloadable']
+        @track.url            = json['permalink_url']
+        @track.streaming_url  = json['stream_url']
+        @track.about          = json['description']
+        @track.small_art_url  = json['artwork_url']
+        @track.large_art_url  = json['artwork_url']
+        @track.artist         = json['user']['username']
+        @track.e_id           = json['id']
+        @track.e_band_id      = json['user-id']
+
+        @tags.each do |tag|
+          @track.taggings.build(:tag_id => tag.id)
+        end
+
+        @track.save
+      end
     else
       @track = Track.new
 

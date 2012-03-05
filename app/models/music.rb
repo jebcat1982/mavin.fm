5fe7685ff766fa92137b053c665f1ddd345b8d7c
Tomer Elmalem <telmalem@gmail.com>
2012-03-04 21:57:10 -0500

    Create a soundcloud track when it's add.

--- app/models/music.rb
+++ app/models/music.rb
@@ -18,6 +18,37 @@ class Music
   end
 
   def soundcloud
+    uri = URI.parse("http://api.soundcloud.com/resolve.json?url=#{self.url}&client_id=#{APIKeys::SOUNDCLOUD}")
+    response = Net::HTTP.get(uri)
+    json = JSON.parse(response)
+    
+    response = Net::HTTP.get(json['location'])
+    json = JSON.parse(response)
+
+    if json['tracks']
+      # soundcloud playlist so create multiple tracks
+    else
+      @track = Track.new
+
+      @track.title          = json['title']
+      @track.duration       = json['duration']
+      @track.downloadable   = json['downloadable']
+      @track.url            = json['permalink_url']
+      @track.streaming_url  = json['stream_url']
+      @track.about          = json['description']
+      @track.small_art_url  = json['artwork_url']
+      @track.large_art_url  = json['artwork_url']
+      @track.artist         = json['user']['username']
+      @track.e_id           = json['id']
+      @track.e_band_id      = json['user-id']
+
+      @tags.each do |tag|
+        @track.taggings.build(:tag_id => tag.id)
+      end
+
+      @track.save
+
+    end
   end
 
   def split_tags

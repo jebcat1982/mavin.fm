0d88d7d95cc47e84c55e95d4fa943ab2772b3cc0
Tomer Elmalem <telmalem@gmail.com>
2012-03-06 16:18:57 -0500

    Change the tags variable name and add the split tags method.

--- app/models/soundcloud.rb
+++ app/models/soundcloud.rb
@@ -1,8 +1,15 @@
 class Soundcloud
-  attr_accessor :url, :tags
+  attr_accessor :url, :raw_tags
 
   def initialize(url = nil, tags = nil)
     self.url  = url
-    self.tags = tags
+    self.raw_tags = tags
+  end
+
+  def split_tags
+    @tags = []
+    raw_tags.split(' ').map do |name|
+      @tags << Tag.find_or_create_by_name(name.chomp)
+    end
   end
 end

dace270ee2e44bdf0ba0031f82b0f127fc8301e1
Tomer Elmalem <telmalem@gmail.com>
2012-03-06 19:50:11 -0500

    Move the soundcloud API/parsing methods to the soundcloud class and call it whenever saving music.

--- app/models/music.rb
+++ app/models/music.rb
@@ -12,48 +12,10 @@ class Music
     if uri.host.index('bandcamp.com')
       b = Bandcamp.new(url, tags)
       b.save
+    elsif uri.host.index('soundcloud.com')
+      s = Soundcloud.new(url, tags)
+      s.save
     end
-
-    soundcloud() if uri.host.index('soundcloud.com')
-  end
-
-  def soundcloud
-    uri = URI.parse("http://api.soundcloud.com/resolve.json?url=#{self.url}&client_id=#{APIKeys::SOUNDCLOUD}")
-    response = Net::HTTP.get(uri)
-    json = JSON.parse(response)
-    
-    response = Net::HTTP.get(json['location'])
-    json = JSON.parse(response)
-
-    if json['tracks']
-      json['tracks'].each do |track|
-        soundcloud_track(track)
-      end
-    else
-      soundcloud_track(json)
-    end
-  end
-
-  def soundcloud_track(json)
-    track = Track.new
-
-    track.title          = json['title']
-    track.duration       = json['duration']
-    track.downloadable   = json['downloadable']
-    track.url            = json['permalink_url']
-    track.streaming_url  = json['stream_url']
-    track.about          = json['description']
-    track.small_art_url  = json['artwork_url']
-    track.large_art_url  = json['artwork_url']
-    track.artist         = json['user']['username']
-    track.e_id           = json['id']
-    track.e_band_id      = json['user-id']
-
-    @tags.each do |tag|
-      track.taggings.build(:tag_id => tag.id)
-    end
-
-    track.save
   end
 
   def split_tags
--- app/models/soundcloud.rb
+++ app/models/soundcloud.rb
@@ -12,4 +12,43 @@ class Soundcloud
       @tags << Tag.find_or_create_by_name(name.chomp)
     end
   end
+  
+  def save 
+    uri = URI.parse("http://api.soundcloud.com/resolve.json?url=#{self.url}&client_id=#{APIKeys::SOUNDCLOUD}")
+    response = Net::HTTP.get(uri)
+    json = JSON.parse(response)
+    
+    response = Net::HTTP.get(json['location'])
+    json = JSON.parse(response)
+
+    if json['tracks']
+      json['tracks'].each do |track|
+        soundcloud_track(track)
+      end
+    else
+      soundcloud_track(json)
+    end
+  end
+
+  def soundcloud_track(json)
+    track = Track.new
+
+    track.title          = json['title']
+    track.duration       = json['duration']
+    track.downloadable   = json['downloadable']
+    track.url            = json['permalink_url']
+    track.streaming_url  = json['stream_url']
+    track.about          = json['description']
+    track.small_art_url  = json['artwork_url']
+    track.large_art_url  = json['artwork_url']
+    track.artist         = json['user']['username']
+    track.e_id           = json['id']
+    track.e_band_id      = json['user-id']
+
+    @tags.each do |tag|
+      track.taggings.build(:tag_id => tag.id)
+    end
+
+    track.save
+  end
 end

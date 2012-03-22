3a1521f17cbf4f1871e2c1b9b3c7a46c78e3882b
Tomer Elmalem <telmalem@gmail.com>
2012-03-21 21:48:05 -0400

    Create a class method for autocompleting tag names.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :albums, :through => :taggings
   has_many :tracks, :through => :taggings
+
+  def self.autocomplete_name(partial)
+    self.where('name LIKE ?', "%#{partial}%").select([:id, :name])
+  end
 end

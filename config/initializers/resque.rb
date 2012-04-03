ecba8d8f18355ef4828d45efdfee5fbcd1daa94c
Tomer Elmalem <telmalem@gmail.com>
2012-04-03 14:40:48 -0400

    Establish a new connection after resque forks.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,3 +3,5 @@ Resque.redis = Discovery.redis
 Dir[Rails.root.join('app', 'jobs', '**', '*.rb')].each do |file| 
   require file
 end
+
+Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }

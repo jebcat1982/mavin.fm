20c6c7555c9fe4cb2f2a1794da68bb0c0257323e
Tomer Elmalem <telmalem@gmail.com>
2012-04-13 11:09:32 -0400

    Add playlist -> tags association.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,7 @@ class Playlist < ActiveRecord::Base
   belongs_to :user
   has_many :playlist_tracks
   has_many :tracks, :through => :playlist_tracks
+
+  has_many :taggings
+  has_many :tags, :through => :taggings, :uniq => true
 end

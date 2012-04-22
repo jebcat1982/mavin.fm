4e3a20a5eed2600c3033e451378d51a0ee9d6d90
Tomer Elmalem <telmalem@gmail.com>
2012-04-21 20:33:26 -0400

    After a user is created find all of the playlists, likes, and dislikes with that session id and set it to that user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,33 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :session_id
 
   has_many :playlists
   has_many :likes
   has_many :dislikes
   has_many :tracks, :through => :likes
   has_many :tracks, :through => :dislikes
+  
+  after_create :assign_all_to_user
+
+  def assign_all_to_user
+    playlists = Playlist.where(session_id: session_id)
+    playlists.each do |playlist|
+      playlist.user_id = id
+      playlist.save
+    end
+
+    dislikes = Dislike.where(session_id: session_id)
+    dislikes.each do |dislike|
+      dislike.user_id = id
+      dislike.save
+    end
+
+    likes = Like.where(session_id: session_id)
+    likes.each do |like|
+      like.user_id = id
+      like.save
+    end
+  end
 end

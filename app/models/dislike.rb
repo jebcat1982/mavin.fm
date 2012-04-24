259f6a0b3ee54953bc75ec385d79d9ed3c41a3e3
Tomer Elmalem <telmalem@gmail.com>
2012-04-23 23:23:08 -0400

    Validate uniqueness of likes/dislikes.

--- app/models/dislike.rb
+++ app/models/dislike.rb
@@ -1,4 +1,6 @@
 class Dislike < ActiveRecord::Base
   belongs_to :user
   belongs_to :track
+
+  validates :track_id, :uniqueness => { :scope => [:user_id, :session_id] }
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :track
+
+  validates :track_id, :uniqueness => { :scope => [:user_id, :session_id] }
 end

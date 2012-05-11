f637723799111cada0adc1e68d1b6b03e5f16e8e
Tomer Elmalem <telmalem@gmail.com>
2012-05-11 15:26:00 -0400

    Delete the old like/dislike methods.

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -4,38 +4,4 @@ class ListenController < ApplicationController
   def index
     @likes = Rating.where(liked: true).order('created_at DESC').limit(5)
   end
-
-  def likes
-    if current_user
-      check = Like.where(user_id: current_user.id, track_id: params[:track_id]).first
-      Like.create(user_id: current_user.id, track_id: params[:track_id])
-    else
-      check = Like.where(session_id: current_session, track_id: params[:track_id]).first
-      Like.create(session_id: current_session, track_id: params[:track_id])
-    end
-
-    if check.nil?
-      playlist = Playlist.find(params[:playlist_id])
-      playlist.like(params[:track_id])
-    end
-
-    render :nothing => true
-  end
-
-  def dislikes
-    if current_user
-      check = Dislike.where(user_id: current_user.id, track_id: params[:track_id])
-      Dislike.create(user_id: current_user.id, track_id: params[:track_id])
-    else
-      check = Dislike.where(session_id: current_session, track_id: params[:track_id])
-      Dislike.create(session_id: current_session, track_id: params[:track_id])
-    end
-
-    if check.nil?
-      playlist = Playlist.find(params[:playlist_id])
-      playlist.dislike(params[:track_id])
-    end
-
-    render :nothing => true
-  end
 end

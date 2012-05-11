2bed1b8f9f45710879730195eaa0959adaf4b73a
Tomer Elmalem <telmalem@gmail.com>
2012-05-11 17:00:18 -0400

    Add a registered boolean flag to user to determine whether they've actually registered or not.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120507185202) do
+ActiveRecord::Schema.define(:version => 20120511203534) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(:version => 20120507185202) do
     t.string   "username"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "registered"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

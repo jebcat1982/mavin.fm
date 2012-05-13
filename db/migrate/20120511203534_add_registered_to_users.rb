86f0d149a3c6df3db444947fc77daea31e25b724
Tomer Elmalem <telmalem@gmail.com>
2012-05-13 17:48:03 -0400

    Remove a bit from the registered migration.

--- db/migrate/20120511203534_add_registered_to_users.rb
+++ db/migrate/20120511203534_add_registered_to_users.rb
@@ -2,10 +2,4 @@ class AddRegisteredToUsers < ActiveRecord::Migration
   def change
     add_column :users, :registered, :boolean
   end
-
-  users = User.all
-  users.each do |user|
-    user.registered = true
-    user.save
-  end
 end

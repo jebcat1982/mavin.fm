ebac20a3d7ef32c9ac9ed6e76b0d7a8b737376c9
Tomer Elmalem <telmalem@gmail.com>
2012-05-13 16:19:23 -0400

    Update the migration to set all of the currently registered users to true.

--- db/migrate/20120511203534_add_registered_to_users.rb
+++ db/migrate/20120511203534_add_registered_to_users.rb
@@ -1,6 +1,11 @@
 class AddRegisteredToUsers < ActiveRecord::Migration
   def change
     add_column :users, :registered, :boolean
+  end
 
+  users = User.all
+  users.each do |user|
+    user.registered = true
+    user.save
   end
 end

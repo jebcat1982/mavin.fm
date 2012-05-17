8a2f6843b5bdb4e42b0c3d289fccc0ad642ef605
Tomer Elmalem <telmalem@gmail.com>
2012-05-17 13:47:33 -0400

    Find and create a known artist from last.fm

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,28 @@ class SearchController < ApplicationController
   end
 
   def find
+    if params[:query].index('by') && params[:query].index('by') > 1
+      # Find track or album
+    else
+      # Find artist
+      artist = params[:query].gsub(' ', '+')
+      uri = URI.parse("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=#{artist}&api_key=#{APIKeys::LASTFM}&autocorrect=1&format=json")
+      resp = Net::HTTP.get(uri)
+      json = JSON.parse(resp)
+
+      artist = KnownArtist.find_or_create_by_name(json['toptags']['@attr']['artist'])
+      
+      json['toptags']['tag'].each do |t|
+        tag = Tag.find(:first, :conditions => [ "lower(name) = ? ", t['name']])
+        unless tag.nil?
+          artist.tags << tag
+        end
+      end
+
+      artist.save
+      path = artist.name.gsub(' ', '+')
+    end
+
     redirect_to search_path
   end
 

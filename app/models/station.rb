13d029c31922d5180d3e73af4351fe576fdf1bae
Tomer Elmalem <telmalem@gmail.com>
2012-05-29 14:44:41 -0400

    Add assosiations to stations.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,8 @@
 class Station < ActiveRecord::Base
+  belongs_to :user
+  has_many :station_tracks
+  has_many :tracks, :through => :station_tracks
+
+  has_many :taggings
+  has_many :tags, :through => :taggings, :uniq => true
 end

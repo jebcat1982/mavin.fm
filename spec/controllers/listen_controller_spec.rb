307f792dabd646e4f50e166394ee74ee271cf79b
Tomer Elmalem <telmalem@gmail.com>
2012-05-30 17:11:07 -0400

    Remove pending on the listen index controller.

--- spec/controllers/listen_controller_spec.rb
+++ spec/controllers/listen_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ListenController do
-  pending "GET 'index'" do
+  describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success

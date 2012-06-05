48e6702a6ae79f6ad3d31c2602a2c1ada80a2c0c
Tomer Elmalem <telmalem@gmail.com>
2012-06-05 13:04:09 -0400

    Refactor station tests a little bit...

--- spec/controllers/stations_controller_spec.rb
+++ spec/controllers/stations_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe StationsController do
+  before(:each) do
+    user = User.new(username: 'tomer', email: 'tomer@awesome.com', registered: false)
+    user.save(validate: false)
+    controller.stub current_user: user
+  end
+
   describe "GET 'index'" do
     it "returns http success" do
       get 'index', :format => :json
@@ -10,9 +16,8 @@ describe StationsController do
 
   describe "GET 'show'" do
     it "returns the selected station" do
-      user = User.create(username: 'tomer', email: 'tomer@awesome.com', registered: false)
-      controller.stub current_user: user
       station = FactoryGirl.create(:station, user_id: controller.current_user.id)
+
       get 'show', format: :json, id: station.id
       response.body.should == station.to_json
     end
@@ -20,19 +25,12 @@ describe StationsController do
 
   describe "POST 'create'" do
     it "creates a station" do
-      user = User.new(username: 'tomer', email: 'tomer@awesome.com', registered: false)
-      user.save(validate: false)
-      controller.stub current_user: user
-
       expect {
         post 'create', format: :json, :station => { name: "Tomer's Music Playlist" }
       }.to change(Station, :count).by(1)
     end
 
     it "creates a station with tags" do
-      user = User.new(username: 'tomer', email: 'tomer@awesome.com', registered: false)
-      user.save(validate: false)
-      controller.stub current_user: user
       FactoryGirl.create(:tag)
 
       expect {

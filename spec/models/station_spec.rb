989ebf2a6ab5f4810d8201dc8c968ff9b20cc16a
Tomer Elmalem <telmalem@gmail.com>
2012-06-05 13:46:33 -0400

    Test that tags are getting created for stations when a genre is provided.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Station do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "callbacks" do
+    it "should create taggings for tag associations" do
+      FactoryGirl.create(:tag)
+
+      station = Station.new(genre: 'jazz')
+      station.save
+      station.tags.count.should == 1
+    end
+  end
 end

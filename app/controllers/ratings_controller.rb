3b27cec44bc1092ae5764b292b01cd4e03a7077e
Tomer Elmalem <telmalem@gmail.com>
2012-06-07 15:34:19 -0400

    Turn rating variable into instance variable.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,24 +2,24 @@ class RatingsController < ApplicationController
   def rate
     liked = params[:liked] == "true" ? true : false
 
-    rating = Rating.where(user_id: current_user.id, track_id: params[:track_id], playlist_id: params[:playlist_id]).first
+    @rating = Rating.where(user_id: current_user.id, track_id: params[:track_id], station_id: params[:station_id]).first
 
-    if rating.nil?
-      rating = current_user.ratings.create(track_id: params[:track_id], playlist_id: params[:playlist_id], liked: liked, time: params[:time], percentage: params[:percentage])
+    if @rating.nil?
+      @rating = current_user.ratings.create(track_id: params[:track_id], station_id: params[:station_id], liked: liked, time: params[:time], percentage: params[:percentage])
 
-      playlist = Playlist.find(params[:playlist_id])
+      station = Station.find(params[:station_id])
       if liked
-        playlist.like(params[:track_id])
+        station.like(params[:track_id])
       else
-        playlist.dislike(params[:track_id])
+        station.dislike(params[:track_id])
       end
     else
-      if rating.liked != liked
-        rating.liked = !rating.liked
-        rating.time = params[:time]
-        rating.percentage = params[:percentage]
+      if @rating.liked != liked
+        @rating.liked = !@rating.liked
+        @rating.time = params[:time]
+        @rating.percentage = params[:percentage]
       end
-      rating.save
+      @rating.save
     end
 
     render nothing: true
